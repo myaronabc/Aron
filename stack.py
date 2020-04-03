5d610a9c0b01da70d4d20d1c98c6fc9b0f6c1bbb
myaronabc <lanxianyangabc@163.com>
2020-04-03 20:59:42 +0800

    update1

--- stack.py
+++ stack.py
@@ -4,3 +4,6 @@ class Stack():
     ''' 定义 stack 类 '''
     def __init__(self):
         self.items = []         #初始化空栈，栈使用列表表示
+
+    def push(self,items):
+        self.items.append(items)        #将items压栈
